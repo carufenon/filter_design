1eaf0d1dd2569de8894a57fa8f7641982fab6c0c
carufenon <fenon1119y@gmail.com>
2024-01-09 23:24:47 +0900

    [add]fftのサンプル処理を追加

--- calc_filter.js
+++ calc_filter.js
@@ -14,12 +14,49 @@ function displayData(data, listId) {
 }
 
 
+function fftfreq(n, d) {
+  let x = []
+  if (n % 2 === 0) {
+      for (let i =0; i < n/2; i++) {
+          x[i] = i * d / n
+      }
+      for (let i =n/2; i < n; i++) {
+          x[i] = (i - n) * d / n
+      }
+  } else {
+      for (let i =0; i < (n-1)/2 + 1; i++) {
+          x[i] = i * d / n
+      }
+      for (let i =(n-1)/2 + 1; i < n; i++) {
+          x[i] = (i - n) * d / n
+      }
+  }
+  return x
+}
+
+
+
+
 // この文があると２回実行されるため削除した。
 // document.getElementById('calculateButton').addEventListener('click', calculateAndVisualize);
 
 // 初期のデータを用意
 const initialXData = [...Array(100).keys()];
 const initialYData = initialXData.map(x => Math.sin(x / 10.0));
+
+
+const t = math.range(0, 256).toArray()
+const y = t.map(function (x){
+  return math.sin(x)
+  })
+sp = math.fft(y)
+freq = fftfreq(256, 1)
+
+
+const initialXData2 = freq
+const initialYData2 = math.abs(sp)
+
+console.log(sp)
 
 // 1st graph
 var layout1 = {
@@ -46,8 +83,8 @@ layout1, {displayModeBar: false});
 
 // 2つ目のグラフも初回描画
 Plotly.newPlot('second-graph', [{
-  x: initialXData,
-  y: initialYData,
+  x: initialXData2,
+  y: initialYData2,
   type: 'scatter',
   mode: 'lines',
 }],
@@ -91,13 +128,13 @@ function calculateAndVisualize() {
   console.log(fircoef_lpf)
 
   // tapn2 + 1 ～ filter_length分の連番配列を生成
-  const newXData = [...Array(Number(filter_length))].map((_, i) => i - tapn2 + 1)
-  const newYData = fircoef_lpf
+  const newXData1 = [...Array(Number(filter_length))].map((_, i) => i - tapn2 + 1)
+  const newYData1 = fircoef_lpf
 
   // 既存のグラフを更新
   Plotly.update('filter-visualization', {
-    x: [newXData],
-    y: [newYData],
+    x: [newXData1],
+    y: [newYData1],
   });
 
 
@@ -105,10 +142,13 @@ function calculateAndVisualize() {
   displayData(fircoef_lpf, 'resultList');
 
 
+  const newXData2 = freq
+  const newYData2 = math.abs(sp)
+
   // 2つ目のグラフも更新
   Plotly.update('second-graph', {
-    x: [newXData],
-    y: [newYData],
+    x: [newXData2],
+    y: [newYData2],
   });
 
 
